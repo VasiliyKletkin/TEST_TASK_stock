033174bae0d6ea4f88e1abd0bd0a1f2346de648a
Vasiliy Kletkin <v.kletkin@mail.ru>
2022-10-10 18:51:42 +0300

    ''

---
+++ order/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Order(models.Model):
+    STATUS = [('New', 'New'),
+              ('In Process', 'In Process'),
+              ('Stored', 'Stored'),
+              ('Send', 'Send')]
+    number = models.CharField(max_length=100, verbose_name='Номер заказа')
+    status = models.CharField(
+        max_length=20, choices=STATUS, default='New', verbose_name='Статуc')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f' Order number {self.order_number} have status {self.status}'
